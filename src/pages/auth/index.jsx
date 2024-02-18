110833d73e3764c69f3023f7ae6d7f8c299d37d2
Olga <udaltsovaolya@gmail.com>
2024-02-18 12:49:11 +0300

    Add auth

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typo } from "../../components/ui/Typo";
 import { Container } from "../../components/ui/Container";
+import { Form } from "../../components/ui/Form";
+import { Field } from "../../components/ui/Field";
+import { Input } from "../../components/ui/Input";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { login } from "../../redux/slices/authSlice";
 
-export const AuthPage = () => (
-  <Container>
-    <Typo>Страница авторизации</Typo>
-  </Container>
-);
+export const AuthPage = () => {
+  const [formValues, setFormValues] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const users = JSON.parse(localStorage.getItem("users"));
+      if (!users) {
+        alert("Данный пользователь не найден в системе");
+        return;
+      }
+
+      const currentUser = users.find(
+        (user) =>
+          user.email === formValues.email &&
+          user.password === formValues.password
+      );
+      if (!currentUser) {
+        alert("Данный пользователь не найден в системе");
+        return;
+      }
+
+      dispatch(login(currentUser));
+      navigate("/posts");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const disabled = !formValues.email || !formValues.password;
+
+  return (
+    <Container>
+      <Typo>Страница авторизации</Typo>
+      <Form onSubmit={onSubmit} autoComplete="off">
+        <Field>
+          <Input
+            type="email"
+            name="email"
+            value={formValues.email}
+            placeholder="Email"
+            onChange={(e) => onChange(e.target.name, e.target.value)}
+          />
+        </Field>
+        <Field>
+          <Input
+            type="password"
+            name="password"
+            value={formValues.password}
+            placeholder="Пароль"
+            onChange={(e) => onChange(e.target.name, e.target.value)}
+            autoComplete="off"
+          />
+        </Field>
+        <button type="submit" disabled={disabled}>
+          Авторизация
+        </button>
+      </Form>
+    </Container>
+  );
+};
--- src/pages/posts/detail/index.jsx
+++ src/pages/posts/detail/index.jsx
@@ -16,8 +16,11 @@ export const DetailPostPage = () => {
   const dispatch = useDispatch();
   const { list } = useSelector((state) => state.posts.posts);
   const postForView = useSelector((state) => state.posts.postForView);
+  const {user} = useSelector((state) => state.auth)
   const [postForDelete, setPostForDelete] = useState(null);
   const navigate = useNavigate();
+
+  const showEditAndDeleteBtn = list && user
 
   const onDeletePost = () => {
     dispatch(deletePost(postForDelete));
@@ -72,8 +75,8 @@ export const DetailPostPage = () => {
       <div style={{ clear: "both" }} />
       <SC.LinkWrapper>
         <Link to="/posts">Обратно к публикациям</Link>
-        {list && <Link to={`/posts/${post.id}/edit`}>Редактировать</Link>}
-        {list && (
+        {showEditAndDeleteBtn && <Link to={`/posts/${post.id}/edit`}>Редактировать</Link>}
+        {showEditAndDeleteBtn && (
           <SC.DeleteButton onClick={() => setPostForDelete(post)}>
             Удалить
           </SC.DeleteButton>
